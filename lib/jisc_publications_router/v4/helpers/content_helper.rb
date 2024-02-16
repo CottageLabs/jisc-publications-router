022df0a018d5c53324b25b25ced6947a5bc36ce4
Anusha Ranganathan <anusha@cottagelabs.com>
2024-02-16 12:48:42 +0530

    Check filename does not exists before writing download

--- lib/jisc_publications_router/v4/helpers/content_helper.rb
+++ lib/jisc_publications_router/v4/helpers/content_helper.rb
@@ -7,8 +7,18 @@ module JiscPublicationsRouter
         include ::JiscPublicationsRouter::V4::Helpers::NotificationHelper
         private
         def _content_path(notification_id, content_link, original_filename)
-          File.join(_notification_path(notification_id),
-                    _content_filename(content_link, original_filename))
+          content_filename = _content_filename(content_link, original_filename)
+          base_path = _notification_path(notification_id)
+          base_name = File.basename(content_filename, ".*")
+          extension = File.extname(content_filename)
+          file_path = File.join(base_path, content_filename)
+          suffix = 1
+          while File.exist?(file_path)
+            new_content_filename = "#{base_name}_#{suffix}#{extension}"
+            file_path = File.join(base_path, new_content_filename)
+            suffix += 1
+          end
+          file_path
         end
 
         def _content_filename(content_link, original_filename)
